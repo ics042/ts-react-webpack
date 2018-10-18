b240063a7a3175578b8955e7f0af7d50b73600b1
ics042 <ics0425907@gmail.com>
2018-10-18 23:02:44 +1300

    add tslint

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
+import "App/assets/scss/main.scss";
 import { Hello } from "App/components/Hello";
-import 'App/assets/scss/main.scss';
 
 ReactDOM.render(
-    <Hello compiler="TypeScript 3" framework="React 16" />,
-    document.getElementById("app")
+  <Hello compiler="TypeScript 3" framework="React 16" />,
+  document.getElementById("app"),
 );
