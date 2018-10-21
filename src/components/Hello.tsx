addaa3243b13f6170b30ea36c3b8fa5e6a8bb802
its404 <benwk424@gmail.com>
2018-10-21 22:40:22 +1300

    add husky

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -8,7 +8,7 @@ export interface IHelloProps {
 // 'IHelloProps' describes the shape of props.
 // State is never set so we use the '{}' type.
 export class Hello extends React.Component<IHelloProps, {}> {
-  public render() {
+  public render(): JSX.Element {
     return (
       <h1>
         Hello from {this.props.compiler} and {this.props.framework}!
